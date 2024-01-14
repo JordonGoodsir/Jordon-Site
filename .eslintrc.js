305f7f7db978afdf839d06b9c2f41e9903350476
Jordon Goodsir <Jordon.goodsir1@gmail.com>
2024-01-14 14:26:43 +1100

    excluded eslintrc from tsconfig

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,39 +1,39 @@
 module.exports = {
-    "parser": "vue-eslint-parser",
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "plugin:vue/vue3-essential",
-        "standard-with-typescript",
-    ],
-    "overrides": [
-        {
-            "env": {
-                "node": true,
-                'vue/setup-compiler-macros': true,
-            },
-            "files": [
-                ".eslintrc.{js,cjs,vue,ts}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parserOptions": {
-        "parser": "@typescript-eslint/parser",
-        "ecmaVersion": "latest",
-        "sourceType": "module",
-        "project": "./tsconfig.json",
-        "extraFileExtensions": [".vue"]
-    },
-    "plugins": [
-        "vue"
-    ],
-    "rules": {
-        "vue/multi-word-component-names": "off",
-        "@typescript-eslint/no-floating-promises": "off"
+  parser: 'vue-eslint-parser',
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    'standard-with-typescript'
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+        'vue/setup-compiler-macros': true
+      },
+      files: [
+        '.eslintrc.{vue,ts}'
+      ],
+      parserOptions: {
+        sourceType: 'script'
+      }
     }
+  ],
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+    extraFileExtensions: ['.vue']
+  },
+  plugins: [
+    'vue'
+  ],
+  rules: {
+    'vue/multi-word-component-names': 'off',
+    '@typescript-eslint/no-floating-promises': 'off'
+  }
 }
