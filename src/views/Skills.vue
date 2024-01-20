3aa6e5500ed6db23d4918b626f199d1ad89b1ace
Jordon Goodsir <Jordon.goodsir1@gmail.com>
2024-01-20 12:32:36 +1100

    good

--- src/views/Skills.vue
+++ src/views/Skills.vue
@@ -1,7 +1,7 @@
 <template>
     <PageWrapper>
       <!-- Page being re designed in figma -->
-      <!-- more design progression -->
+      <!-- more design progression + 2 -->
         <div class="flex flex-col w-full items-center">
 
           <div class="flex items-center justify-between bg-[#F0F9F] h-[300px] w-full border-b-2 border-black">
